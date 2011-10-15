34bad40a567eaf4a32d71d6e8111d40904f77939
Paul Mow <pmow@honolulu.gov>
2011-10-14 14:03:27 -1000

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,9 @@
+class CommentsController < ApplicationController
+  # POST /comments
+  # POST /comments.xml
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(params[:comment])
+    redirect_to @post
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBlog::Application.config.secret_token = 'eae96a943d354b30005c045166fbac81499368831c4a0f3787ea59dd4c8e946f0931e30bf3db251eb1a150f7ebce95de403eea80cea934a4ef5bc01d9cce1fc7'
